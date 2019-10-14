f9ac76aa78d20de8fc7da251061b9a2268d4e4f9
Takayuki Miyahara <voyager.3taka28@gmail.com>
2019-10-14 15:25:01 +0900

    test: add smoke test

--- spec/fcmpush_spec.rb
+++ spec/fcmpush_spec.rb
@@ -1,9 +1,27 @@
 RSpec.describe Fcmpush do
-  it "has a version number" do
+  let(:device_token) { ENV.fetch('TEST_DEVICE_TOKEN') }
+  let(:project_id) { ENV.fetch('TEST_PROJECT_ID') }
+
+  it 'has a version number' do
     expect(Fcmpush::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'simple smoke test' do
+    client = Fcmpush.new(project_id)
+    message_json = {
+      message: {
+        token: device_token,
+        notification: {
+          title: 'test title',
+          body: 'test body'
+        }
+      }
+    }
+    response = client.push(message_json)
+
+    json = response.json
+
+    expect(response.code).to eq('200')
+    expect(json[:name]).to start_with("projects/#{project_id}/messages/")
   end
 end
