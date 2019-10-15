52e6e354982d67cec65fe71fbc837b9fd9365eeb
Takayuki Miyahara <voyager.3taka28@gmail.com>
2019-10-16 00:53:37 +0900

    refactor: devide files

---
+++ lib/fcmpush/json_response.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+module Fcmpush
+  class JsonResponse < DelegateClass(Net::HTTPResponse)
+    alias response __getobj__
+    alias headers to_hash
+    HAS_SYMBOL_GC = RUBY_VERSION > '2.2.0'
+
+    def json
+      parsable? ? JSON.parse(body, symbolize_names: HAS_SYMBOL_GC) : nil
+    end
+
+    def inspect
+      "#<JsonResponse response: #{response.inspect}, json: #{json}>"
+    end
+    alias to_s inspect
+
+    def parsable?
+      !body.nil? && !body.empty?
+    end
+  end
+end
