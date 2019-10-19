d58ef4b923d70bddede319aabd2fbbdbe1145390
Takayuki Miyahara <voyager.3taka28@gmail.com>
2019-10-19 16:46:30 +0900

    test: add subscribe/unsubscribe case

--- spec/fcmpush_spec.rb
+++ spec/fcmpush_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Fcmpush do
   let(:device_token) { ENV.fetch('TEST_DEVICE_TOKEN') }
   let(:project_id) { ENV.fetch('TEST_PROJECT_ID') }
+  let(:server_key) { ENV.fetch('FCM_SERVER_KEY') }
 
   it 'has a version number' do
     expect(Fcmpush::VERSION).not_to be nil
@@ -23,5 +24,25 @@ RSpec.describe Fcmpush do
 
     expect(response.code).to eq('200')
     expect(json[:name]).to start_with("projects/#{project_id}/messages/")
+  end
+
+  it 'subscribe test' do
+    client = Fcmpush.new(project_id)
+    response = client.subscribe('/topics/test_topic', device_token)
+
+    json = response.json
+
+    expect(response.code).to eq('200')
+    expect(json[:results]).to eq([{}])
+  end
+
+  it 'unsubscribe test' do
+    client = Fcmpush.new(project_id)
+    response = client.unsubscribe('/topics/test_topic', device_token)
+
+    json = response.json
+
+    expect(response.code).to eq('200')
+    expect(json[:results]).to eq([{}])
   end
 end
