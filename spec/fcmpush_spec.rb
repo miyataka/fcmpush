b97d0de66f378503270b87485fcc30ab9bfc5f60
Takayuki Miyahara <voyager.3taka28@gmail.com>
2020-08-19 11:30:32 +0900

    feat: separate configuration object between clients

--- spec/fcmpush_spec.rb
+++ spec/fcmpush_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe Fcmpush do
   let(:project_id) { ENV.fetch('TEST_PROJECT_ID') }
   let(:server_key) { ENV.fetch('FCM_SERVER_KEY') }
   let(:json_key_path) { ENV.fetch('TEST_JSON_KEY_PATH') }
+  let(:another_json_key_path) { ENV.fetch('ANOTHER_TEST_JSON_KEY_PATH') }
 
   it 'has a version number' do
     expect(Fcmpush::VERSION).not_to be nil
@@ -146,6 +147,21 @@ RSpec.describe Fcmpush do
 
         expect(response.code).to eq('200')
         expect(json[:name]).to start_with("projects/#{project_id}/messages/")
+      end
+    end
+
+    context 'configuration is isolate between clients' do
+      it 'smoke test' do
+        Fcmpush.configure do |config|
+          config.json_key_io = json_key_path
+        end
+        client_a = Fcmpush.new(project_id)
+
+        Fcmpush.configure do |config|
+          config.json_key_io = another_json_key_path
+        end
+        client_b = Fcmpush.new(project_id)
+        expect(client_a.configuration.object_id).not_to eq(client_b.configuration.object_id)
       end
     end
   end
